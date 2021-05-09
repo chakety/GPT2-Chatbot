89376ae90e41e6747138e04df21104dd9b2b7eb1
zeyucheng <43229231+zeyucheng@users.noreply.github.com>
2021-05-09 15:40:39 -0700

    Update main.py

--- main.py
+++ main.py
@@ -10,23 +10,18 @@ app.static_folder = 'static'
 
 # set the hyper-paramters
 parser = argparse.ArgumentParser(description='GPT-2 chatbot')
-parser.add_argument('--nsamples', type=int, default=1,
-                    help='set number of bot outputs')
+parser.add_argument('--nsamples', type=int, default=1)
 
-parser.add_argument('--top_k', type=int, default=40,
-                    help='set limited to only number of k words in order of highest probability')
+parser.add_argument('--top_k', type=int, default=40)
 
-parser.add_argument('--top_p', type=int, default=0.9,
-                    help='set sum probability p that only words exceeding p are put in the candidate')
+parser.add_argument('--top_p', type=int, default=0.7)
 
-parser.add_argument('--temperature', type=float, default=0.9,
-                    help='write flexibly if the temperature is high, and write statically if the temperature is low (0.0 ~ 1.0)')
+parser.add_argument('--temperature', type=float, default=1.0)
 
-parser.add_argument('--batch_size', type=int, default=1,
-                    help='set the batch size')
+parser.add_argument('--batch_size', type=int, default=1)
 
-parser.add_argument('--length', type=int, default=80,
-                    help='set the response maximum number of length')
+parser.add_argument('--length', type=int, default=400)
+
 args = parser.parse_args()
 
 @app.route("/")
